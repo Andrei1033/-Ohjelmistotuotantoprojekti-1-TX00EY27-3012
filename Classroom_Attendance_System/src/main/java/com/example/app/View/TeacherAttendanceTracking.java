19f1d7b2f4a8d8a35b34314f06025333314fb96c
Khaled <khaled-marai1@hotmail.com>
2026-09-12 13:57:07 +0300

    Lisätään opettajan läsnäolon seurantanäkymä ja sivupalkki

--- Classroom_Attendance_System/src/main/java/com/example/app/View/TeacherAttendanceTracking.java
+++ Classroom_Attendance_System/src/main/java/com/example/app/View/TeacherAttendanceTracking.java
@@ -1,4 +1,70 @@
 package com.example.app.View;
 
-public class TeacherAttendanceTracking {
+
+
+
+
+// SIDEBAR COLOR
+// #202F49 - navy
+
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class TeacherAttendanceTracking extends BorderPane {
+
+    // Colors
+    private  static final String NAVY = "#202F49";
+
+    // Sidebar creation
+    private static final int PRESENT = 1;
+    private static final int ABSENT = 2;
+    private static final int LATE = 3;
+
+
+
+    // Constructor
+    public TeacherAttendanceTracking(Runnable onFinish) {
+        setStyle(
+                "-fx-background-color: white;"
+        );
+        // Left sidebar
+        VBox sidebar = createSidebar();
+        setLeft(sidebar);
+        // Main content
+        VBox content = createMainContent(onFinish);
+        setCenter(content);
+    }
+
+    private VBox createMainContent(
+            Runnable onFinish
+    ) {
+        VBox content = new VBox();
+
+        content.setPadding(
+                new Insets(
+                        40, 40, 40, 40
+                )
+        );
+        content.setStyle(
+                "-fx-background-color: white;"
+        );
+        return content;
+    }
+
+
+    private VBox createSidebar() {
+
+        VBox sidebar = new VBox();
+        sidebar.setPrefWidth(220);
+        sidebar.setMaxWidth(220);
+        sidebar.setMinWidth(220);
+
+        sidebar.setStyle(
+                "-fx-background-color: " + NAVY + ";"
+        );
+        return sidebar;
+    }
+
 }
